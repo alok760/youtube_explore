d117e258df0b55056a645613c9de6668e11c2a4d
alok <alok760@gmail.com>
2022-01-02 10:29:09 +0530

    initialize frontend

--- youtube_explore/models.py
+++ youtube_explore/models.py
@@ -7,11 +7,11 @@ class Video(models.Model):
     # release_date = models.DateField()
     # num_stars = models.IntegerField()
     # TODO: add indexes
-    video_id = models.CharField(max_length=100)
+    video_id = models.CharField(max_length=100, primary_key=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
     etag = models.CharField(max_length=100) #signifies change
     channel_id = models.CharField(max_length=100)
-    channel_title = models.CharField(max_length=100)
-    published_at = models.DateTimeField(max_length=100)
-    thumbnail_url = models.CharField(max_length=100)
+    channel_title = models.CharField(max_length=100, db_index=True)
+    published_at = models.DateTimeField(max_length=100, null=True, db_index=True)
+    thumbnail_url = models.CharField(max_length=100, null=True)
--- youtube_explore/views.py
+++ youtube_explore/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Video
+from django.core import serializers
+from datetime import datetime
 import requests
 
 def index(request):
@@ -16,8 +18,8 @@ def index(request):
         'publishedAfter': '2020-01-01T00:00:00Z',
         'q': 'education'
     }
-    response = requests.get('https://youtube.googleapis.com/youtube/v3/search', headers=headers, params=params)
-    response_json = response.json()
+    # response = requests.get('https://youtube.googleapis.com/youtube/v3/search', headers=headers, params=params)
+    # response_json = response.json()
     from .response import response_json
     for item in response_json['items']:
         video = Video(
@@ -27,12 +29,31 @@ def index(request):
             etag = item['etag'],#signifies change
             channel_id = item['snippet']['channelId'],
             channel_title = item['snippet']['channelTitle'],
-            # publishedAt = 
-            # thumbnail_url = 
+            published_at = datetime.strptime(item['snippet']['publishedAt'], "%Y-%m-%dT%H:%M:%SZ"),
+            thumbnail_url = item['snippet']['thumbnails']['default']['url']
         )
         video.save()
+        breakpoint()
     return HttpResponse("Hello, World!")
 
+
+
+def get(request):
+    page = int(request.GET['page'])
+    page_size = int(request.GET['page_size'])
+    OFFSET = page_size * (page - 1)
+    LIMIT = page_size
+    query = Video.objects.all()[OFFSET:OFFSET+LIMIT]
+    response_data = serializers.serialize('json', query)
+    return HttpResponse(response_data, content_type='application/json')
+
 def search(request):
-    pass
+    page = int(request.GET['page'])
+    page_size = int(request.GET['page_size'])
+    OFFSET = page_size * (page - 1)
+    LIMIT = page_size
+    query = Video.objects.all()[OFFSET:OFFSET+LIMIT]
+    response_data = serializers.serialize('json', query)
+    return HttpResponse(response_data, content_type='application/json')
+
 # Create your views here.
